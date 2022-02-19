a92f9dd43a01fe872fb23170228cd90b2f79cf5e
Sujit <bhoirsujit04>
2022-02-19 22:33:33 +0530

    only one left

--- app/src/main/java/com/gameside/savestatus/FlashActivity.java
+++ app/src/main/java/com/gameside/savestatus/FlashActivity.java
@@ -127,6 +127,7 @@ public class FlashActivity extends AppCompatActivity {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 recreate();
+
             } else {
                 finish();
             }
